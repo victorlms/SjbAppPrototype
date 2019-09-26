798fafbeb9f06d5d90c0d83aa5ec7ddd01937950
Victor Lima <victor.lms00@gmail.com>
2019-09-25 22:24:15 -0300

    Ajuste nos imports das páginas dentro do Routes, finalização da estrutura básica da aba de Agenda, sendo necessário alguns ajustes, inclusão de mais itens na bottombar, início da construção da página de destaques

---
+++ app/pages/Destaque.js
@@ -0,0 +1,173 @@
+import React, {Component} from 'react';
+import {
+  View,
+  FlatList,
+  Picker,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import globalStyles from '../styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+var language;
+var data = [
+  {title: 1, key: 'a'},
+  {title: 2, key: 'b'},
+  {title: 3, key: 'c'},
+  {title: 4, key: 'd'},
+  {title: 5, key: 'e'},
+];
+
+export default class Destaque extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderItem(item, index) {
+    return item.title == 1 ? (
+      <View style={styles.pickContainer}>
+        <Image
+          style={styles.leftImage}
+          source={require('../assets/agenda/item1.png')}
+        />
+        <Text>4º Festival</Text>
+      </View>
+    ) : item.title === 2 ? (
+      <View style={styles.pickContainer}>
+        <Image
+          style={styles.leftImage}
+          source={require('../assets/agenda/item2.png')}
+        />
+        <Text>Passeata</Text>
+      </View>
+    ) : item == 3 ? (
+      <View style={styles.pickContainer}>
+        <Image
+          style={styles.leftImage}
+          source={require('../assets/agenda/item3.png')}
+        />
+        <Text>Último dia</Text>
+      </View>
+    ) : item === 4 ? (
+      <View style={styles.pickContainer}>
+        <Image
+          style={styles.leftImage}
+          source={require('../assets/agenda/item4.png')}
+        />
+        <Text>Degustação</Text>
+      </View>
+    ) : (
+      <View
+        style={{
+          ...styles.pickContainer,
+          height: 130,
+          marginBottom: 1,
+          backgroundColor: '#FFF',
+          padding: 12,
+        }}>
+        <Image
+          style={styles.leftImage}
+          source={require('../assets/agenda/item5.png')}
+        />
+        <Text style={styles.listText}>Início da votações para prefeito</Text>
+        <View style={{flex: 2, justifyContent: 'center'}}>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 25,
+              fontFamily: 'helvetica',
+              fontWeight: 'bold',
+              color: '#828282',
+            }}>
+            16
+          </Text>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 25,
+              fontWeight: 'bold',
+              color: '#828282',
+            }}>
+            ----
+          </Text>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 25,
+              fontWeight: 'bold',
+              color: '#828282',
+            }}>
+            AGO
+          </Text>
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={{width: '100%'}}>
+        <View style={styles.pickContainer}>
+          <Text
+            style={{
+              fontSize: 25,
+              color: 'white',
+              fontWeight: 'bold',
+              flex: 4,
+              padding: 10,
+            }}>
+            O que quer saber?
+          </Text>
+          <TouchableOpacity style={{height: 40, flex: 1}}>
+            <Image
+              style={{width: 40, height: 40}}
+              source={require('../assets/agenda/menu.png')}
+            />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <FlatList data={data} renderItem={this.renderItem}></FlatList>
+        </View>
+        <View
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            zIndex: -1,
+            backgroundColor: 'lightgray',
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  pickContainer: {
+    width: '100%',
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 10,
+    backgroundColor: '#0013a3',
+  },
+  pick: {
+    flex: 5,
+  },
+  leftImage: {
+    margin: 10,
+    width: 60,
+    height: 60,
+    flex: 1.5,
+  },
+  listText: {
+    flex: 5,
+    fontWeight: '100',
+    marginLeft: 5,
+    justifyContent: 'center',
+    textAlignVertical: 'center',
+    fontSize: 20,
+  },
+});
--- app/pages/SJB.js
+++ app/pages/SJB.js
@@ -1,94 +1,117 @@
-import React, { Component } from "react";
-import {
-    StyleSheet,
-    View,
-    Text,
-} from "react-native";
+import React, {Component} from 'react';
+import {StyleSheet, FlatList, Picker, View, Text} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import Swiper from 'react-native-swiper'
-import globalStyles from "../styles";
+import Swiper from 'react-native-swiper';
+import globalStyles from '../styles';
 import BottomNavigation, {
-    FullTab
+  FullTab,
 } from 'react-native-material-bottom-navigation';
-
-import {Agenda} from './Agenda'
+import Agenda from './Agenda';
+import Destaque from './Destaque';
 
 const tabs = [
-    {
-        key: 'edit',
-        icon: 'edit',
-        label: 'Edit',
-        barColor: '#388E3C',
-        pressColor: 'rgba(255, 255, 255, 0.16)'
-    },
-    {
-        key: 'movies-tv',
-        icon: 'movie',
-        label: 'Movies & TV',
-        barColor: '#B71C1C',
-        pressColor: 'rgba(255, 255, 255, 0.16)'
-    },
-    {
-        key: 'music',
-        icon: 'music-note',
-        label: 'Music',
-        barColor: '#E64A19',
-        pressColor: 'rgba(255, 255, 255, 0.16)'
-    }
-]
+  {
+    key: 'book',
+    icon: 'book',
+    label: '',
+    barColor: '#FF4500',
+    pressColor: 'rgba(255, 255, 255, 0.16)',
+  },
+  {
+    key: 'map',
+    icon: 'map',
+    label: '',
+    barColor: '#388E3C',
+    pressColor: 'rgba(255, 255, 255, 0.16)',
+  },
+  {
+    key: 'news',
+    icon: 'speaker-notes',
+    label: '',
+    barColor: '#0013a3',
+    pressColor: 'rgba(255, 255, 255, 0.16)',
+  },
+  {
+    key: 'calendar',
+    icon: 'calendar-today',
+    label: '',
+    barColor: '#d9002f',
+    pressColor: 'rgba(255, 255, 255, 0.16)',
+  },
+  {
+    key: 'trend',
+    icon: 'fireplace',
+    label: '',
+    barColor: '#ffc800',
+    pressColor: 'rgba(255, 255, 255, 0.16)',
+  },
+];
 
 export default class SJB extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    activeTab = tabs[0].key;
+  activeTab = tabs[0].key;
 
-    renderIcon = icon => ({ isActive }) => (
-        <Icon size={24} color="white" name={icon} />
-    )
+  renderIcon = icon => ({isActive}) => (
+    <Icon size={24} color="white" name={icon} />
+  );
 
-    renderTab = ({ tab, isActive }) => (
-        <FullTab
-            isActive={isActive}
-            key={tab.key}
-            label={tab.label}
-            renderIcon={this.renderIcon(tab.icon)}
+  renderTab = ({tab, isActive}) => (
+    <FullTab
+      isActive={isActive}
+      key={tab.key}
+      label={tab.label}
+      renderIcon={this.renderIcon(tab.icon)}
+    />
+  );
+
+  setTab = ({key}) => {
+    const actualIndex = tabs.findIndex(el => el.key === this.activeTab);
+    const nextIndex = tabs.findIndex(el => el.key === key);
+
+    this.swiper.scrollBy(nextIndex - actualIndex);
+    this.activeTab = key;
+  };
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <Swiper
+          scrollEnabled={false}
+          ref={ref => (this.swiper = ref)}
+          style={globalStyles.containerCenterFlex}
+          loop={false}
+          showsButtons={false}
+          showsPagination={false}>
+          <Agenda />
+          <View
+            style={{
+              ...globalStyles.containerCenterFlex,
+              backgroundColor: '#0F0',
+            }}>
+            <Text style={globalStyles.title}>Movie</Text>
+          </View>
+          
+          <Destaque />
+          <View
+            style={{
+              ...globalStyles.containerCenterFlex,
+              backgroundColor: '#0F0',
+            }}>
+            <Text style={globalStyles.title}>MUsic</Text>
+          </View>
+        </Swiper>
+        <BottomNavigation
+          ref={ref => (this.bottomNav = ref)}
+          style={{width: '100%'}}
+          onTabPress={this.setTab}
+          renderTab={this.renderTab}
+          tabs={tabs}
+          useLayoutAnimation
         />
-    )
-
-    setTab = ({ key }) => {
-        const actualIndex = tabs.findIndex(el => el.key === this.activeTab);
-        const nextIndex = tabs.findIndex(el => el.key === key);
-        
-        this.swiper.scrollBy( nextIndex - actualIndex );
-        this.activeTab = key;
-    }
-    
-
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <Swiper scrollEnabled={ false } ref={ ref => this.swiper = ref } style={ globalStyles.containerCenterFlex } loop={ false } showsButtons={ false } showsPagination={ false }>
-                    <View style={{ ...globalStyles.containerCenterFlex}}>
-                       <Agenda/>
-                    </View>
-                    <View style={{ ...globalStyles.containerCenterFlex, backgroundColor: "#0F0" }}>
-                        <Text style={globalStyles.title}>Movie</Text>
-                    </View>
-                    <View style={{ ...globalStyles.containerCenterFlex, backgroundColor: "#0F0" }}>
-                        <Text style={globalStyles.title}>MUsic</Text>
-                    </View>
-                </Swiper>
-                <BottomNavigation
-                    ref={ ref => this.bottomNav = ref }
-                    style={{ width: "100%" }}
-                    onTabPress={ this.setTab }
-                    renderTab={ this.renderTab }
-                    tabs={ tabs }
-                    useLayoutAnimation
-                />
-            </View>
-        )
-    }
+      </View>
+    );
+  }
 }
